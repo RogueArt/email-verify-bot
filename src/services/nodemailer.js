9bf3d1e70243efe27b4aca23d43982f7158a65f2
RogueArt <altoraries@gmail.com>
2022-08-03 22:06:16 -0700

    Add email templating to send email using Nodemailer

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -2,21 +2,35 @@
 import nodemailer from 'nodemailer'
 import config from '../../config.js'
 
+import ejs from 'ejs'
+import fs from 'fs'
+import url from 'url'
 const { emailConfig } = config
 
 const transporter = nodemailer.createTransport({
-    service: process.env.SENDER_SERVICE,
-    auth: {
-      user: process.env.SENDER_EMAIL,
-      pass: process.env.SENDER_PASS, // generated ethereal password
-    },
-  })
+  service: process.env.SENDER_SERVICE,
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_PASS, // generated ethereal password
+  },
+})
 
-async function sendAuthEmail(authCode, email) {
+function getEmailTemplate(authCode) {
+  // Get file path
+  const templateURL = new URL(emailConfig.emailTemplatePath, import.meta.url)
+  const filePath = url.fileURLToPath(templateURL.href)
+
+  // Read and template the HTML
+  const htmlContent = fs.readFileSync(filePath, 'utf-8')
+  const htmlRenderized = ejs.render(htmlContent, { authCode })
+  return htmlRenderized
+}
+
+export async function sendAuthEmail(authCode, email) {
   return await transporter.sendMail({
     from: emailConfig.from, // sender address
     to: email, // list of receivers
     subject: emailConfig.subject, // Subject line
-    html: '<html><b>Hello world to you good sir!</b></html>', // html body
+    html: getEmailTemplate(authCode), // html body
   })
 }
